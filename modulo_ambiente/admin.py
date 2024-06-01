f5c6ebf4969cead498496fef124c29674e96d8c1
dianaguerrero <dguerreropacheco@gmail.com>
2024-06-01 16:13:45 -0500

    horEsc

--- modulo_ambiente/admin.py
+++ modulo_ambiente/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import edificio, tipo_ambiente, ambiente
+from .models import edificio, tipo_ambiente, ambiente, horarioEscuela
 # Register your models here.
 admin.site.register(edificio)
 admin.site.register(tipo_ambiente)
 admin.site.register(ambiente)
+admin.site.register(horarioEscuela)
 
 admin.site.site_header="Administración del Sistema Generador de Horarios-USAT"
 admin.site.site_title="Administración del Sistema Generador de Horarios-USAT"
