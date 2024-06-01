10c9f2e2e4e982aafd7202cdecdf31052dab9f7d
DianaSuclupe <dsuclupeg@gmail.com>
2024-05-31 22:51:32 -0500

    nuevo

--- modulo_horario/views.py
+++ modulo_horario/views.py
@@ -3,8 +3,21 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def saludo(request):
+def inicio(request):
     return render(request,'index.html')
+
+def menuHorario(request):
+    return render(request,'menuHorario.html')
+
+def menuCurso(request):
+    return render(request,'menuCurso.html')
+
+def menuDocente(request):
+    return render(request,'menuDocente.html')
+
+def menuAmbiente(request):
+    return render(request,'menuAmbiente.html')
+
 
 def horario(request):
     return HttpResponse("horarios de los docentes")
